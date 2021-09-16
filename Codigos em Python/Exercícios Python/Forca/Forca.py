c8fe68447310fba9609a35d09624f156f21698a7
Daniel Camargo <drubiocamargo@gmail.com>
2021-09-16 19:16:12 -0300

    Jogo da Forca simples criado!

---
+++ Codigos em Python/Exercícios Python/Forca/Forca.py
@@ -0,0 +1,29 @@
+palavra = str(input("Insira a palavra : "))
+listaLetras = list()
+listaCodificada = list()
+erros = 0
+cont = 0
+for c in palavra:
+    listaLetras.append(c)
+    listaCodificada.append("*")
+digitados = list()
+while erros < 12:
+    cont += 1
+    print("{}\nPalavra : {}\n{}".format("-"*20,"".join(listaCodificada),"-"*20))
+    tentativa = str(input("Insira sua tentativa : "))
+    if tentativa in digitados:
+        print("Você ja digitou essa letra!")
+    elif len(tentativa) > 1:
+        print("Insira uma letra e não uma palavra!")
+    else:
+        for i,c in enumerate(listaLetras):
+            if c == tentativa:
+                listaCodificada[i] = tentativa
+                digitados.append(tentativa)
+            elif tentativa not in listaLetras:
+                erros += 1
+    if "*" not in listaCodificada:
+        print("\033[1;32mVocê venceu!!!\033[m")
+        break
+print("\033[1;33mA palavra era {}\033[m".format(palavra))
+print("\033[1;33mVocê levou {} tentativas para tentar acertar\033[m".format(cont))
