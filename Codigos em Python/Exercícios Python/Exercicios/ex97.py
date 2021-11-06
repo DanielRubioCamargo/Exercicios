531a65a7d140a66ee52f7249b498bc72ac2a91cb
Daniel Camargo <drubiocamargo@gmail.com>
2021-11-06 17:57:12 -0300

    Função recursiva adicionada!

--- Codigos em Python/Exercícios Python/Exercicios/ex97.py
+++ Codigos em Python/Exercícios Python/Exercicios/ex97.py
@@ -4,7 +4,12 @@ def fatorial(numero):
     return numero * fatorial(numero - 1)
 
 def main():
-    print(fatorial(5))
+    print(pot(5,2))
+
+def pot(number,exp):
+    if exp == 0:
+        return 1
+    return number * pot(number,exp-1)
 
 if __name__ == "__main__":
     main()
