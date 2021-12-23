bddbb37b8cdf9358fca7d1c81ac886cca90e50ce
Daniel Camargo <drubiocamargo@gmail.com>
2021-12-23 17:25:35 -0300

    Notas!

---
+++ Codigos em Python/Finale/main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import display
+
+gradesDF = pd.read_excel("Codigos em Python\\Finale\\Notas Python.xlsx")
+
+xValues = []
+yValues = []
+
+while True:
+    print("1 -> Notas 1\n2 -> Notas 2\n3 -> Medias\n4 -> Sair")
+    option = int(input("Insira sua opção : "))
+    if option == 1:
+        yValues = gradesDF["Nota 1"]
+    elif option == 2:
+        yValues = gradesDF["Nota 2"]
+    elif option == 3:
+        yValues = gradesDF["Media"]
+    elif option == 4:
+        break
+    else:
+        print("Inválido")
+        continue
+    title = str(input("Insira um titulo : "))
+    xValues = gradesDF["Nome"]
+    plt.bar(xValues,yValues)
+    plt.title(title)
+    plt.show()
+    
