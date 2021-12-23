05a49b748042b0c92f892077b787fdce409e4030
Daniel Camargo <drubiocamargo@gmail.com>
2021-12-23 17:06:42 -0300

    Testes!

--- Codigos em Python/Exercícios Python/Exercicios/ex96.py
+++ Codigos em Python/Exercícios Python/Exercicios/ex96.py
@@ -1,15 +1,21 @@
-def fatorial(numero):
-    if numero == 1:
-        return 1
-    return numero * fatorial(numero-1)
-
-def potencia(numero,expoente):
-    if expoente == 0:
-        return 1
-    return numero * potencia(numero,expoente-1)
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+k = int(input())
+s = str(input())
+lst = s.split(" ")
+auxLst = []
+contLst = []
+for c in lst:
+    c = int(c)
+for c in lst:
+    if c not in auxLst:
+        auxLst.append(c)
+for c in auxLst:
+    contLst.append(0)
+for c in lst:
+    for i,d in enumerate(auxLst):
+        if c == d:
+            contLst[i]+=1
+for i,c in enumerate(contLst):
+    if c == 1:
+        print(auxLst[i])
+        break
--- Codigos em Python/Exercícios Python/Exercicios/ex99.py
+++ Codigos em Python/Exercícios Python/Exercicios/ex99.py
@@ -1,18 +1,32 @@
+def opcoes_lista(lista:list):
+    listaOpcoes = ["Ordem crescente","Ordem decrescente","Impares","Pares","Maiores que um número"]
+    for i,c in enumerate(listaOpcoes):
+        print("{} -> {}".format(i+1,c))
+    op = int(input("Qual a sua opção? : "))
+    if op == 1:
+        print(sorted(lista))
+    elif op == 2:
+        print(sorted(lista,reverse=True))
+    elif op == 3:
+        for c in lista:
+            if c%2==1:
+                print(c)
+    elif op == 4:
+        for c in lista:
+            if c%2==0:
+                print(c)
+    elif op == 5:
+        valor = int(input("Qual será o valor : "))
+        for c in lista:
+            if c > valor:
+                print(c)
+    else: 
+        print("Opção inválida")
 
 
-# Complete the solve function below.
-def solve(s):
-    stringList = s.split(" ")
-    lastList = list()
-    for c in stringList:
-        lastList.append(c.capitalize())
-    return " ".join(lastList)
+def main():
+    lista = [1, 1, 2, 3, 5, 8, 13, 21]
+    opcoes_lista(lista)
 
-if __name__ == '__main__':
-
-    s = input()
-
-    result = solve(s)
-    print(result)
-
-    
+if __name__ == "__main__":
+    main()
