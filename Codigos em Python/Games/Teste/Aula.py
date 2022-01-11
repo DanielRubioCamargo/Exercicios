a6939a0cdd9aa19d9032de3c8e0a2b983e1246da
Daniel Camargo <drubiocamargo@gmail.com>
2022-01-11 15:24:51 -0300

    Game!

--- Codigos em Python/Games/Teste/Aula.py
+++ Codigos em Python/Games/Teste/Aula.py
@@ -1,22 +1,73 @@
 import pygame as pg
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pg.init()
 
 width = 640
 heigth = 480
+xRed = width/2
+yRed = heigth/2
+xBlue = 300
+yBlue = 200
 
+points = 0
+font = pg.font.SysFont("arial",40,True,True)
 screen = pg.display.set_mode((width,heigth))
 gameName = pg.display.set_caption("Game")
-
-pg.draw.rect(screen,(255,0,0),(50,400,60,60))
-pg.draw.circle(screen,(0,0,255),(width/2,heigth/2),50)
-pg.draw.line(screen,(255,255,50),(0,0),(300,20),3)
+clock = pg.time.Clock()
 
 while True:
+    clock.tick(30)
+    screen.fill((0,0,0))
+    message = "Pontos : {}".format(points)
+    text = font.render(message,True,(255,255,255))
     for event in pg.event.get():
         if event.type == QUIT:
             pg.quit()
             exit()
+            '''
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                x = x - 20
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20
+                '''
+               
+    redRect = pg.draw.rect(screen,(255,0,0),(xRed,yRed,60,60))
+    blueRect = pg.draw.rect(screen,(0,0,255),(xBlue,yBlue,60,60))
+    
+    if pg.key.get_pressed()[K_a]:
+        xRed = xRed - 20
+    if pg.key.get_pressed()[K_d]:
+        xRed = xRed + 20
+    if pg.key.get_pressed()[K_w]:
+        yRed = yRed - 20
+    if pg.key.get_pressed()[K_s]:
+        yRed = yRed + 20
+
+    if xRed > width:
+        xRed = 0
+    if xRed < 0:
+        xRed = width-60
+    if yRed > heigth:
+        yRed = 0
+    if yRed < 0:
+        yRed = heigth-60
+    
+
+    if redRect.colliderect(blueRect):
+        xBlue = randint(0,590)
+        yBlue = randint(0,430)
+        points+=1
+
+    screen.blit(text,(400,50))
+
     pg.display.update()
+    
+
--- Codigos em Python/Games/Teste/teste.py
+++ Codigos em Python/Games/Teste/teste.py
@@ -1,22 +1,35 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-
+ 
 pygame.init()
-
-SCREEN_WIDTH = 640
-SCREEN_HEIGTH = 480
-
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGTH))
-gameName = pygame.display.set_caption("Game")
-
-pygame.draw.rect(screen,(255,255,255),(200,300,30,40))
-pygame.draw.circle(screen,(255,0,0),(50,50),40)
-pygame.draw.line(screen,(255,255,0),(30,30),(300,300),5)
-
+ 
+largura = 640
+altura = 480
+y = altura/2
+x = largura/2
+ 
+tela = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('PYGAME')
+frame = pygame.time.Clock()
+ 
 while True:
+    frame.tick(30) 
+    tela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+    
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                x = x - 20
+            if event.key == K_d:
+                x = x + 20
+            if event.key == K_w:
+                y = y - 20
+            if event.key == K_s:
+                y = y + 20
+        
+    pygame.draw.rect(tela, (255,0,0), (x,y, 40,50))
     pygame.display.update()
