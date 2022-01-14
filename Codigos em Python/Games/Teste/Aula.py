5a9eeaa0408c5d8688ce61e25dad1abfc509886e
Daniel Camargo <drubiocamargo@gmail.com>
2022-01-14 16:13:47 -0300

    Snake Game parte 1!

--- Codigos em Python/Games/Teste/Aula.py
+++ Codigos em Python/Games/Teste/Aula.py
@@ -3,25 +3,31 @@ from pygame.locals import *
 from sys import exit
 from random import randint
 
+def draw_snake(lst : list):
+    for pos in lst:
+        pg.draw.rect(screen,(0,255,0),(pos[0],pos[1],20,20))
+
 pg.init()
 
 width = 640
 heigth = 480
-xRed = width/2
-yRed = heigth/2
-xBlue = 300
-yBlue = 200
+xSnake = width/2
+ySnake = heigth/2
+xApple = 300
+yApple = 200
 
-pg.mixer.music.set_volume(0.45)
+pg.mixer.music.set_volume(0.25)
 backgroundSong = pg.mixer.music.load("Codigos em Python\\Games\\Teste\\Chaves   Tema de Abertura Original Televisa Completo.mp3")
 pg.mixer.music.play(-1)
-collisionSound = pg.mixer.Sound("Codigos em Python\\Games\\Teste\\y2meta.com - cavalo (128 kbps).mp3")
+#collisionSound = pg.mixer.Sound("Codigos em Python\\Games\\Teste\\y2meta.com - cavalo (128 kbps).mp3")
 
 points = 0
 font = pg.font.SysFont("arial",40,True,True)
 screen = pg.display.set_mode((width,heigth))
 gameName = pg.display.set_caption("Game")
 clock = pg.time.Clock()
+
+snakeList = list()
 
 while True:
     clock.tick(30)
@@ -44,33 +50,41 @@ while True:
                 y = y + 20
                 '''
                
-    redRect = pg.draw.rect(screen,(255,0,0),(xRed,yRed,60,60))
-    blueRect = pg.draw.rect(screen,(0,0,255),(xBlue,yBlue,60,60))
+    snake = pg.draw.rect(screen,(0,255,0),(xSnake,ySnake,20,20))
+    apple = pg.draw.rect(screen,(255,0,0),(xApple,yApple,20,20))
+
+    headList = list()
+    headList.append(xSnake)
+    headList.append(ySnake)
+    snakeList.append(headList)
+
+    draw_snake(snakeList)
+
     
     if pg.key.get_pressed()[K_a]:
-        xRed = xRed - 20
+        xSnake = xSnake - 20
     if pg.key.get_pressed()[K_d]:
-        xRed = xRed + 20
+        xSnake = xSnake + 20
     if pg.key.get_pressed()[K_w]:
-        yRed = yRed - 20
+        ySnake = ySnake - 20
     if pg.key.get_pressed()[K_s]:
-        yRed = yRed + 20
+        ySnake = ySnake + 20
 
-    if xRed > width:
-        xRed = 0
-    if xRed < 0:
-        xRed = width-60
-    if yRed > heigth:
-        yRed = 0
-    if yRed < 0:
-        yRed = heigth-60
+    if xSnake > width:
+        xSnake = 0
+    if xSnake < 0:
+        xSnake = width-60
+    if ySnake > heigth:
+        ySnake = 0
+    if ySnake < 0:
+        ySnake = heigth-60
     
 
-    if redRect.colliderect(blueRect):
-        xBlue = randint(0,590)
-        yBlue = randint(0,430)
+    if snake.colliderect(apple):
+        xApple = randint(0,590)
+        yApple = randint(0,430)
         points+=1
-        collisionSound.play()
+        #collisionSound.play()
 
     screen.blit(text,(400,50))
 
