4ccfc762985836c18c47f9165d73ffdf3c27acbb
Daniel Camargo <drubiocamargo@gmail.com>
2022-03-16 14:30:51 -0300

    Mudança de spritesheet e finalização Road Game!

--- Codigos em Python/Games/Road Game/main.py
+++ Codigos em Python/Games/Road Game/main.py
@@ -27,10 +27,26 @@ bgCount2 = -600
 clock = pygame.time.Clock()
 FPS = 30
 
+resetFont = pygame.font.SysFont("Arial",30,True,True)
+resetMessage = "Press 'R' to reset the game!"
+resetText = resetFont.render(resetMessage,True,(255,90,90))
+
+def reset_game():
+    global bgCount1, bgCount2
+    car.hasCollided = False
+    car.rect.x = 220
+    greenCar1.rect.y = -SPRITE_SIZE*2
+    greenCar1.rect.x = choice(greenCar1.xList)
+    greenCar2.rect.y = -SPRITE_SIZE*6
+    greenCar2.rect.x = choice(greenCar1.xList)
+    bgCount1 = 0
+    bgCount2 = -600
+
 class Car(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.xSpeed = 0
+        self.hasCollided = False
         self.hasToMoveRight = False
         self.hasToMoveLeft = False
         self.moveConstant = 0
@@ -38,6 +54,7 @@ class Car(pygame.sprite.Sprite):
         self.initialYpos = (SCREEN_HEIGTH/2) + 100
         self.image = spritesheet.subsurface((0,0),(SPRITE_SIZE,SPRITE_SIZE))
         self.image = pygame.transform.scale(self.image,(SPRITE_SIZE*2,SPRITE_SIZE*2))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = self.initialXpos
         self.rect.y = self.initialYpos
@@ -71,10 +88,11 @@ class Car(pygame.sprite.Sprite):
 class GreenCar(pygame.sprite.Sprite):
     def __init__(self, yPos : float):
         pygame.sprite.Sprite.__init__(self)
-        self.ySpeed = 10
+        self.ySpeed = 8
         self.xList = [-10,220,450]
         self.image = spritesheet.subsurface((SPRITE_SIZE,0),(SPRITE_SIZE,SPRITE_SIZE))
         self.image = pygame.transform.scale(self.image,(SPRITE_SIZE*2,SPRITE_SIZE*2))
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.x = choice(self.xList)
         self.rect.y = yPos
@@ -88,7 +106,7 @@ class GreenCar(pygame.sprite.Sprite):
 spriteGroup = pygame.sprite.Group()
 car = Car()
 greenCar1 = GreenCar(-SPRITE_SIZE*2)
-greenCar2 = GreenCar(-SPRITE_SIZE*5)
+greenCar2 = GreenCar(-SPRITE_SIZE*6)
 spriteGroup.add(car)
 spriteGroup.add(greenCar1)
 spriteGroup.add(greenCar2)
@@ -106,9 +124,8 @@ while True:
                 car.hasToMoveRight = True
             elif event.key == K_a and (car.rect.x != -10) and car.hasToMoveRight == False:
                 car.hasToMoveLeft = True
-
-    bgCount1 += 10
-    bgCount2 += 10
+            if event.key == K_r and car.hasCollided == True:
+                reset_game()
 
     if bgCount1 == 600:
         bgCount1 = -600
@@ -118,5 +135,13 @@ while True:
     screen.blit(backgroundImage,(0,bgCount1))
     screen.blit(backgroundImage2,(0,bgCount2))
     spriteGroup.draw(screen)
-    spriteGroup.update()
+
+    if pygame.sprite.collide_mask(car,greenCar1) or pygame.sprite.collide_mask(car,greenCar2):
+        car.hasCollided = True
+        screen.blit(resetText,(140,200))
+    
+    if car.hasCollided == False:
+        bgCount1 += 50
+        bgCount2 += 50
+        spriteGroup.update()
     pygame.display.flip()
