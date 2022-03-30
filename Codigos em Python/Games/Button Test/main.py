f293ce4cfc13ca244ff2eda9d5f113272966ff6a
Daniel Camargo <drubiocamargo@gmail.com>
2022-03-30 14:43:31 -0300

    Botões Pygame!

---
+++ Codigos em Python/Games/Button Test/main.py
@@ -0,0 +1,42 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGTH = 480
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGTH))
+gameTitle = pygame.display.set_caption("Button Class")
+
+clock = pygame.time.Clock()
+FPS = 30
+
+GREEN = (0,255,0)
+RED = (255,0,0)
+
+hasClicked = False
+color = RED
+
+while True:
+    clock.tick(FPS)
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+    mousePos = pygame.mouse.get_pos()
+    button = pygame.draw.rect(screen,color,(100,200,200,50))
+
+    if button.collidepoint(mousePos):
+        if pygame.mouse.get_pressed()[0] and hasClicked == False:
+            color = GREEN
+            print("Clicou")
+            hasClicked = True
+        
+    if pygame.mouse.get_pressed()[0] == False:
+        color = RED
+        hasClicked = False
+
+    pygame.display.flip()
